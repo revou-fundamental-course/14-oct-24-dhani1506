1773793a8ebba018ea71c05042da017c60bd22f0
dhani1506 <mridhani51@gmail.com>
2024-10-24 00:23:02 +0800

    up article

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 
     // let user = prompt("Ketik Nama Anda")
-    //document.getElementById('user').innerHTML = user
+    // document.getElementById('user').innerHTML = user
 
     // Message
-
     function validateForm () {
         const name = document.forms ["message-form"]["nama-panjang"].value;
         const date = document.forms ["message-form"]["tanggal-lahir"].value;
@@ -24,6 +23,29 @@
         document.getElementById('hasil-tanggal-lahir').innerHTML = date;
         document.getElementById('hasil-gender').innerHTML = gender;
         document.getElementById('hasil-pesan').innerHTML = message;
+    }
+
+    // Banner Slide
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n)
+    }
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName('banner');
+        if (n > x.length) {slideIndex = 1} ;
+        if (n < 1) {slideIndex = x.length}
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none" ;
+        }
+        x[slideIndex-1].style.display = "block";
+        // Interval
+        setInterval(() => {
+            plusDivs(1);
+        }, 5000);
     }
 
 
